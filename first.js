0af34a198b9cba2dbb6fca5e5faf9f4add110ae9
chandan jayas <90899405+chandanjayas@users.noreply.github.com>
2022-01-26 12:40:30 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=100
+var c=50
+if((a>b) && (a>c)){
+console.log("a is greatest number");
+}
+else if((b>a) && (b>c)){
+console.log("b is greatest number");
+}
+else{
+console.log("c is also greatest number");
+}
+
+
+
